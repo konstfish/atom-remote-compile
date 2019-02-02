22c503772c05cbf66cecf2800b59e1ea2aa2c288
David Fischer <david@konst.fish>
2019-02-02 21:10:16 +0100

    code cleanup, add already running, remove view

--- spec/remote-compile-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import RemoteCompileView from '../lib/remote-compile-view';
-
-describe('RemoteCompileView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
